e735995f9261b2d59752553eac6ce5a4c170576e
Idan Levin <67044900+Idan-Levin@users.noreply.github.com>
2024-07-24 17:21:38 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <Box bg="white" px={8} py={4} borderBottom="1px" borderColor="gray.200">
       <Flex maxW="1200px" mx="auto" alignItems="center">
-        <Image src="/logo.png" alt="OpenCommerce Logo" height="30px" />
+        <Image src="/opencommerce-logo.png" alt="OpenCommerce Logo" height="30px" />
         <Button colorScheme="purple" variant="solid" size="md" rounded="full" ml="auto">
           Log In
         </Button>
